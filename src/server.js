29f85cea29ad589d2228987358ccbbbe6b22ca0d
nazar <road2thedream1@gmail.com>
2026-03-19 02:56:07 +0100

    first check at autocheck

--- src/server.js
+++ src/server.js
@@ -1,57 +1,33 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
-import pino from 'pino-http';
+
+
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { errorHandler } from './middleware/erroeHandler.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
+
 app.use(express.json());
 app.use(cors());
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
+app.use(logger);
 
-app.get('/notes',(req,res)=>{
-  res.status(200).json({
-	"message": "Retrieved all notes"
-  });
-});
-app.get('/notes/:noteId',(req,res)=>{
-  const {noteId} = req.params;
-  res.status(200).json(
-    {
-	"message": `Retrieved note with ID: ${noteId}`
-  }
-  );
-});
-app.get('/test-error',(req,res)=>{
-  throw new Error('Simulated server error');
-});
-app.use((req,res)=>{
-  res.status(404).json({
-    message: 'Route not found'
-  });
-});
-app.use((err, req, res, next) => {
-  console.error('Error:', err.message);
-  res.status(500).json({
-    message: 'Internal Server Error',
-    error: err.message,
-  });
-});
+
+
+app.use(notesRoutes);
+
+app.use(errorHandler);
+app.use(notFoundHandler);
+
+await connectMongoDB();
+
+
 app.listen(PORT,()=>{console.log(`Server is running on port ${PORT}`);});
 
 
