7e7a1fce8232a967c2a82f0d2d06fb0d95800592
nazar <road2thedream1@gmail.com>
2026-03-23 11:22:56 +0100

    fixed Tags,joi.string

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -3,7 +3,7 @@ import { TAGS } from "../constants/tags.js";
 import { isValidObjectId } from 'mongoose';
 
 const objectIdValidator = (value, helpers) => {
-  return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
+  return !isValidObjectId(value) ? helpers.message("Invalid MongoDB ObjectId") : value;
 };
 
 export const getAllNotesSchema={[Segments.QUERY]: Joi.object({
@@ -13,7 +13,7 @@ export const getAllNotesSchema={[Segments.QUERY]: Joi.object({
   search: Joi.string().allow('').trim()
 })};
 export const noteIdSchema={[Segments.PARAMS]: Joi.object({
-  noteId: Joi. string().custom(objectIdValidator).required()
+  noteId: Joi.string().custom(objectIdValidator).required()
 })};
 
 export const createNoteSchema={[Segments.BODY]: Joi.object({
@@ -23,7 +23,7 @@ export const createNoteSchema={[Segments.BODY]: Joi.object({
 })};
 export const updateNoteSchema={
   [Segments.PARAMS]:Joi.object({
-      noteId: Joi. string().custom(objectIdValidator).required()
+      noteId: Joi.string().custom(objectIdValidator).required()
   }),
   [Segments.BODY]:Joi.object({
     title: Joi.string().min(1).optional(),
