c213968658220474cf01096e28547afad09e6580
nazar <road2thedream1@gmail.com>
2026-03-23 11:28:33 +0100

    fixed search optional

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -10,10 +10,10 @@ export const getAllNotesSchema={[Segments.QUERY]: Joi.object({
   page: Joi.number().integer().min(1).default(1),
   perPage: Joi.number().integer().min(5).max(20).default(10),
   tag:Joi.string().valid(...TAGS).optional(),
-  search: Joi.string().allow('').trim()
+  search: Joi.string().allow('').trim().optional()
 })};
 export const noteIdSchema={[Segments.PARAMS]: Joi.object({
-  noteId: Joi.string().custom(objectIdValidator).required()
+  noteId: Joi.string().custom(objectIdValidator).required().trim()
 })};
 
 export const createNoteSchema={[Segments.BODY]: Joi.object({
@@ -23,7 +23,7 @@ export const createNoteSchema={[Segments.BODY]: Joi.object({
 })};
 export const updateNoteSchema={
   [Segments.PARAMS]:Joi.object({
-      noteId: Joi.string().custom(objectIdValidator).required()
+      noteId: Joi.string().custom(objectIdValidator).required().trim()
   }),
   [Segments.BODY]:Joi.object({
     title: Joi.string().min(1).optional(),
