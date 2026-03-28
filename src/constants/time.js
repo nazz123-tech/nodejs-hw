d194e1a7f73c918eb4c75da115916ef0b3c9b5dc
nazar <road2thedream1@gmail.com>
2026-03-29 00:10:28 +0100

    middleware and models connect left

---
+++ src/constants/time.js
@@ -0,0 +1,2 @@
+export const FIFTEEN_MINUTES=15*60*1000;
+export const ONE_DAY=60*24*60*1000;
--- src/server.js
+++ src/server.js
@@ -8,8 +8,9 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
-
+import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -17,9 +18,10 @@ const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 app.use(logger);
 
-
+app.use(authRoutes);
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
