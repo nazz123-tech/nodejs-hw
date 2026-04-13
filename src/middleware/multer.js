68aa4cd7a46556bd841804a77313ea9279d2038b
nazar <road2thedream1@gmail.com>
2026-04-13 11:45:56 +0200

    fixed allowed types multer

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -6,12 +6,10 @@ export const upload = multer({
     fileSize: 2 * 1024 * 1024,
   },
   fileFilter: (req, file, cb) => {
-	  const allowedTypes = 'image/';
-
-	  if (allowedTypes.includes(file.mimetype)) {
-	    cb(null, true);
-	  } else {
-	    cb(new Error('Invalid file type. Only JPEG, PNG, GIF, and WebP are allowed.'), false);
-	  }
+	 if (file.mimetype.startsWith('image/')) {
+  cb(null, true);
+} else {
+  cb(new Error('Invalid file type. Only JPEG, PNG, GIF, and WebP are allowed.'), false);
+}
   },
 });
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -11,7 +11,7 @@ router.post('/auth/register', celebrate(registerUserSchema),registerUser);
 router.post('/auth/login',celebrate(loginUserSchema),loginUser);
 router.post('/auth/logout',logoutUser);
 router.post('/auth/refresh', refreshUserSession);
-router.post('/auth/request-reset-mail', celebrate(requestResetEmailSchema), requestResetEmail);
+router.post('/auth/request-reset-email', celebrate(requestResetEmailSchema), requestResetEmail);
 router.post('/auth/reset-password',celebrate(resetPasswordSchema), resetPassword);
 
 export default router;
