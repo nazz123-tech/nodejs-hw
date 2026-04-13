1cf3ad45a8b084377e16d1f966240221d94fc604
nazar <road2thedream1@gmail.com>
2026-04-13 11:20:45 +0200

    initial

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/authController.js";
+import { loginUser, logoutUser, registerUser, requestResetEmail, resetPassword } from "../controllers/authController.js";
 import { celebrate } from "celebrate";
-import { loginUserSchema, registerUserSchema } from "../validations/authValidation.js";
+import { loginUserSchema, registerUserSchema, requestResetEmailSchema, resetPasswordSchema } from "../validations/authValidation.js";
 import { refreshUserSession } from "../controllers/authController.js";
+import { upload } from "../middleware/multer.js";
 
 const router = Router();
 
@@ -10,5 +11,7 @@ router.post('/auth/register', celebrate(registerUserSchema),registerUser);
 router.post('/auth/login',celebrate(loginUserSchema),loginUser);
 router.post('/auth/logout',logoutUser);
 router.post('/auth/refresh', refreshUserSession);
+router.post('/auth/request-reset-mail', celebrate(requestResetEmailSchema), requestResetEmail);
+router.post('/auth/reset-password',celebrate(resetPasswordSchema), upload.single("avatar"), resetPassword);
 
 export default router;
--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -10,3 +10,14 @@ export const loginUserSchema={[Segments.BODY]: Joi.object({
     password: Joi.string().required(),
 })};
 
+export const requestResetEmailSchema={
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().email().required(),
+  })
+};
+export const resetPasswordSchema={
+  [Segments.BODY]: Joi.object({
+    password: Joi.string().min(8).required(),
+    token: Joi.string().required,
+  })
+};
